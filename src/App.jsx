ef19d8c6a09fc956f681b10da0bbb0d76d76c5a6
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-10-21 10:57:14 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react"
+
+function App() {
+    return (
+        <>
+            <h1 className="bg-rose-500 text-4xl">Test Tailwind</h1>
+        </>
+    )
+}
+
+export default App
