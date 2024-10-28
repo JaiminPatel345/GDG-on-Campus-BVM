36765b2987573bdb046fc20b733ca49a89ed0fd9
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-10-28 20:06:40 +0530

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
 import React from "react"
 import "../styles/general.css"
+import { Boxes } from "../components/ui/background-boxes"
+import { cn } from "../lib/utils"
+import Logo from "/logo_home.png"
 
 const Home = () => {
-    return <div id="home"></div>
+    return (
+        <div
+            className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg"
+            id="HomePage"
+        >
+            <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+
+            <Boxes />
+            {/* <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
+                Google Developers Groups On Campus
+            </h1>
+            <p className="text-center mt-2 text-neutral-300 relative z-20">
+                Birla Vishvakarma Mahavidyalaya
+            </p> */}
+            <img src={Logo} alt="GDGC BVM" className="h-40 relative z-20" />
+        </div>
+    )
 }
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+const tailwindConfig = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Use updated color names
+        // Replace old names with the new ones
+        sky: colors.sky,
+        stone: colors.stone,
+        neutral: colors.neutral,
+        gray: colors.gray,
+        slate: colors.slate,
+
+        // Add any custom colors if needed
+        'custom-blue': '#1e40af', // Example of a custom color
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
 
+export default tailwindConfig;
