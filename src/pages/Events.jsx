c61a4eb47b4a94881e6e88e49d3859ab3fd2b9d7
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-10-30 19:51:55 +0530

    update event section

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react"
 import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card"
 import { Link } from "react-router-dom"
@@ -6,42 +7,68 @@ import { EventCard } from "../components"
 
 const Events = () => {
     return (
-        <div>
-            {/* <h1>Events</h1>
-            <EventCard
-                title="Google Cloud Study GAM"
-                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa."
-                redirect="/event/study-jam"
-                background="https://media.contentapi.ea.com/content/dam/eacom/images/2020/09/ea-featured-image-ea-desktop-beta.jpg.adapt.crop191x100.1200w.jpg"
-            /> */}
+        <div className="flex flex-col mt-36">
+            <div>
+                <h1 className=" text-white text-4xl text-center">Events</h1>
+            </div>
 
-            <ThreeDCardDemo />
+            <div className="flex flex-nowrap gap-9 overflow-y-auto justify-center">
+                <ThreeDCardDemo
+                    title="Google Gen Ai study jam"
+                    description="aslfdkhnasdlkfj asdefljkhbasluifhdjbjk sadkufghasudfhasudjfb sadkfujghaslifhduoisfhdjnfahjfoijwertuiewpoirtyjlkngflkjsdfhgoifrhdgknvn xcfhngoifhsdg lhgifdoisfdj"
+                    imgUrl="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    learnUrl="/events/genai"
+                    registerUrl="/"
+                />
+
+                <ThreeDCardDemo
+                    title="Google Gen Ai study jam"
+                    description="aslfdkhnasdlkfj asdefljkhbasluifhdjbjk sadkufghasudfhasudjfb sadkfujghaslifhduoisfhdjnfahjfoijwertuiewpoirtyjlkngflkjsdfhgoifrhdgknvn xcfhngoifhsdg lhgifdoisfdj"
+                    imgUrl="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    learnUrl="/events/genai"
+                    registerUrl="/"
+                />
+
+                <ThreeDCardDemo
+                    title="Google Gen Ai study jam"
+                    description="aslfdkhnasdlkfj asdefljkhbasluifhdjbjk sadkufghasudfhasudjfb sadkfujghaslifhduoisfhdjnfahjfoijwertuiewpoirtyjlkngflkjsdfhgoifrhdgknvn xcfhngoifhsdg lhgifdoisfdj"
+                    imgUrl="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    learnUrl="/events/genai"
+                    registerUrl="/"
+                />
+            </div>
         </div>
     )
 }
 
 export default Events
 
-const ThreeDCardDemo = () => {
+const ThreeDCardDemo = ({
+    title,
+    description,
+    imgUrl,
+    learnUrl,
+    registerUrl,
+}) => {
     return (
         <CardContainer className="inter-var">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+            <CardBody className="relative group/card  hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black border-white/[0.2]  w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                 <CardItem
                     translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                    className="text-xl font-bold text-white"
                 >
-                    Make things float in air
+                    {title}
                 </CardItem>
                 <CardItem
                     as="p"
                     translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                    className="text-sm max-w-sm mt-2 text-neutral-300"
                 >
-                    Hover over this card to unleash the power of CSS perspective
+                    {description}
                 </CardItem>
                 <CardItem translateZ="100" className="w-full mt-4">
                     <img
-                        src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src={imgUrl}
                         height="1000"
                         width="1000"
                         className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
@@ -52,16 +79,18 @@ const ThreeDCardDemo = () => {
                     <CardItem
                         translateZ={20}
                         as="a"
-                        href="https://twitter.com"
+                        href={learnUrl}
                         target="__blank"
-                        className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                        className="px-4 py-2 rounded-xl text-xs font-normal text-white"
                     >
                         Learn More →
                     </CardItem>
                     <CardItem
                         translateZ={20}
-                        as="button"
-                        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                        as="a"
+                        target="_blank"
+                        href={registerUrl}
+                        className="px-4 py-2 rounded-xl bg-white text-black  text-xs font-bold"
                     >
                         Register
                     </CardItem>
