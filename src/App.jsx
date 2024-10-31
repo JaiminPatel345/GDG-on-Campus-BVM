13ed2a1e0159116b963b3e81b65b44f567be8a78
Narendra <malinarendra7874@gmail.com>
2024-10-31 18:43:31 +0530

    add nav update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import NavBar from "./components/NavBar" // Import the NavBar component
-import { Home, About, Events, Contact, Team, Flashback } from "./pages"
+import NavBar from "./components/NavBar"; // Import the NavBar component
+import { Home, About, Events, Contact, Team, Flashback } from "./pages";
 
 const App = () => {
-    return (
-        <>
-        <div className=" flex  h-screen w-screen object-cover overflow-y-auto overflow-x-hidden">
-            <NavBar />
-            <div className="h-full w-full object-cover overflow-y-auto overflow-x-hidden">
-                <Home></Home>
-                <About></About>
-                <Events></Events>
-                <Contact></Contact>
-                <Team></Team>
-                <Flashback></Flashback>
-
-            </div>
-            
+  return (
+    <>
+      <div className=" flex  h-screen w-screen object-cover overflow-y-auto overflow-x-hidden">
+        <NavBar />
+        <div className="h-full w-full object-cover overflow-y-auto overflow-x-hidden">
+          <Home></Home>
+          <About></About>
+          <Events></Events>
+          <Contact></Contact>
+          <Team></Team>
+          <Flashback></Flashback>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
